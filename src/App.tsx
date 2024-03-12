6cc29907938814e59a51e3f0c7c29ef32f62e22e
anton <kamaeff2@gmail.com>
2024-03-12 21:51:42 +0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import {Header, Main} from './components';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Header />
+      <Main />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+  return <h1>Header</h1>;
+};
+
+export default Header;
