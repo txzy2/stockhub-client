e836549ae46f8118f9df7523be21ec8936a36be6
anton <kamaeff2@gmail.com>
2024-03-12 22:10:04 +0300

    add tg

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './main.scss';
 
+declare const window: any;
+const tg = window.Telegram.WebApp;
+
 const Main = () => {
-  return <div className=''>Main</div>;
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const onClose = () => {
+    tg.close();
+  };
+
+  return (
+    <div className=''>
+      <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
 };
 
 export default Main;
