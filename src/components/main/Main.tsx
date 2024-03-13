fbf7223353801b19000f8ad63f532c67f68018f6
anton <kamaeff2@gmail.com>
2024-03-13 18:14:25 +0300

    fix

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -15,7 +15,7 @@ const Main = () => {
         <div className='main__search--input flex gap-1.5 p-3.5'>
           <Search size={28} />
           <input
-            className='w-62 text-xl outline-none bg-none'
+            className='main__search--input w-62 text-xl outline-none'
             placeholder='Поиск'
           ></input>
         </div>
