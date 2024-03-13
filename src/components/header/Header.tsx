329a992a74aea2a4418396efcd98991e89f1d815
anton <kamaeff2@gmail.com>
2024-03-13 12:26:14 +0300

    remove img logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './header.scss';
 import {UseTg} from '../../hooks/useTg';
-import logo from '../../assets/logo.png';
+// import logo from '../../assets/logo.png';
 import {CircleUser, ShoppingCart} from 'lucide-react';
 
 const Header = () => {
@@ -15,13 +15,14 @@ const Header = () => {
           <span className='header__user_info--name'>{user.first_name}</span>
         </div>
 
-        <img
+        {/* <img
           src={logo}
           alt='user_photo'
           width={220}
           height={98}
           className='header__user--img'
-        />
+        /> */}
+
         <div className='header__user_basket'>
           <ShoppingCart size={32} />
           <div className='header__user_basket--count'>2</div>
