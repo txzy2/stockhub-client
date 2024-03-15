0923ca80a3e518aae119954a49cea24a71fe5b67
anton <kamaeff2@gmail.com>
2024-03-15 13:26:13 +0300

    fix

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -56,8 +56,8 @@ const Main = ({product}: {product: any}) => {
 
       <div className='gap-2 ps-4 pt-3'>
         <h2 className='text-xl font-medium'>User stats:</h2>
-        <p>from chat: {chat_id}</p>
-        <p>from user: {user.id}</p>
+        <p>from chat: {chat_id !== undefined ? chat_id : 'Undefined'}</p>
+        <p>from user: {user?.id !== undefined ? user.id : 'Undefined'}</p>
         {userData ? (
           <>
             <p>
