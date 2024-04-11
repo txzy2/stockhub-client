dc6831f4b687686c3452bbae4fed5474c0c9b77a
anton <kamaeff2@gmail.com>
2024-04-11 16:04:17 +0300

    add selections

--- src/components/main/components/Filter/Filter.tsx
+++ src/components/main/components/Filter/Filter.tsx
@@ -19,6 +19,37 @@ const Filter = ({closeModal}: ModalProps) => {
           </select>
         </label>
       </div>
+
+      <div className='filter__options'>
+        <label className='grid'>
+          Цвет
+          <select className='filter__options--select'>
+            <option value='gray'>Серый</option>
+            <option value='red'>Красный</option>
+          </select>
+        </label>
+      </div>
+
+      <div className='filter__options'>
+        <label className='grid'>
+          Бренд
+          <select className='filter__options--select'>
+            <option value='nike'>Nike</option>
+            <option value='puma'>Puma</option>
+            <option value='jordan'>Jordan</option>
+          </select>
+        </label>
+      </div>
+
+      <div className='filter__options'>
+        <label className='grid'>
+          Откуда
+          <select className='filter__options--select'>
+            <option value='msk'>MCK</option>
+            <option value='poizon'>Poizon</option>
+          </select>
+        </label>
+      </div>
     </div>
   );
 };
