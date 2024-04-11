885db1d747e7782a6e5e544a60b547c7ce509f21
anton <kamaeff2@gmail.com>
2024-04-11 15:54:57 +0300

    start filter

--- src/components/main/components/Filter/Filter.tsx
+++ src/components/main/components/Filter/Filter.tsx
@@ -1,6 +1,8 @@
 import {X} from 'lucide-react';
 import {ModalProps} from '../../../../types/types';
 
+import './filter.scss';
+
 const Filter = ({closeModal}: ModalProps) => {
   return (
     <div className='filter'>
@@ -8,7 +10,15 @@ const Filter = ({closeModal}: ModalProps) => {
         <X className='exit' size={30} />
       </button>
 
-      <div className=''>Filter Works!</div>
+      <div className='filter__options'>
+        <label className='grid'>
+          Одежда / Обувь
+          <select className='filter__options--select'>
+            <option value='cloth'>Одежда</option>
+            <option value='sneaker'>Обувь</option>
+          </select>
+        </label>
+      </div>
     </div>
   );
 };
