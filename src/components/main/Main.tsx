1db84cac245dd203947d8d34b12fc357f1697737
anton <kamaeff2@gmail.com>
2024-04-11 10:26:05 +0300

    add loader

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,4 +1,10 @@
-import {Footprints, Search, Shirt, SlidersHorizontal} from 'lucide-react';
+import {
+  Footprints,
+  Loader,
+  Search,
+  Shirt,
+  SlidersHorizontal,
+} from 'lucide-react';
 import {Carousel} from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
@@ -11,8 +17,10 @@ import {images} from '../../assets/imagesAssets';
 import Filter from './components/Filter/Filter';
 import Shooes from './components/ShooesComponent/Shoes';
 import Cloth from './components/ClothComponent/Cloth';
+import {UseTg} from '../../hooks/useTg';
 
 const Main = () => {
+  const {user} = UseTg();
   const [isFilterOpen, setIsFilterOpen] = useState(false);
 
   const [selectedButton, setSelectedButton] = useState<string | null>('shoes');
@@ -35,71 +43,82 @@ const Main = () => {
 
   return (
     <div className='main'>
-      <section className='main__search'>
-        <div className='main__search--input'>
-          <Search size={28} />
-          <input
-            className='main__search--input_text'
-            placeholder='Поиск'
-          ></input>
-        </div>
-        <button className='main__search--filter' onClick={openFilter}>
-          <SlidersHorizontal size={28} />
-        </button>
-      </section>
+      {user?.id ? (
+        <>
+          <section className='main__search'>
+            <div className='main__search--input'>
+              <Search size={28} />
+              <input
+                className='main__search--input_text'
+                placeholder='Поиск'
+              ></input>
+            </div>
+            <button className='main__search--filter' onClick={openFilter}>
+              <SlidersHorizontal size={28} />
+            </button>
+          </section>
 
-      <div className='main__btn'>
-        <button
-          className={`main__btn-item ${
-            selectedButton === 'clothing' ? 'active' : ''
-          }`}
-          onClick={() => setSelectedButton('clothing')}
-        >
-          <Shirt size={30} />
-          Одежда
-        </button>
+          <div className='main__btn'>
+            <button
+              className={`main__btn-item ${
+                selectedButton === 'clothing' ? 'active' : ''
+              }`}
+              onClick={() => setSelectedButton('clothing')}
+            >
+              <Shirt size={30} />
+              Одежда
+            </button>
 
-        <button
-          className={`main__btn-item ${
-            selectedButton === 'shoes' ? 'active' : ''
-          }`}
-          onClick={() => setSelectedButton('shoes')}
-        >
-          <Footprints />
-          Обувь
-        </button>
-      </div>
+            <button
+              className={`main__btn-item ${
+                selectedButton === 'shoes' ? 'active' : ''
+              }`}
+              onClick={() => setSelectedButton('shoes')}
+            >
+              <Footprints />
+              Обувь
+            </button>
+          </div>
 
-      <section className='main__sections'>
-        <div className='main__carousel'>
-          <Carousel
-            infiniteLoop={true}
-            autoPlay={true}
-            interval={5000}
-            showThumbs={false}
-          >
-            {items}
-          </Carousel>
-        </div>
-      </section>
+          <section className='main__sections'>
+            <div className='main__carousel'>
+              <Carousel
+                infiniteLoop={true}
+                autoPlay={true}
+                interval={5000}
+                showThumbs={false}
+              >
+                {items}
+              </Carousel>
+            </div>
+          </section>
 
-      {selectedButton === 'clothing' && <Cloth />}
+          {selectedButton === 'clothing' && <Cloth />}
 
-      {selectedButton === 'shoes' && <Shooes />}
+          {selectedButton === 'shoes' && <Shooes />}
 
-      <AnimatePresence>
-        {isFilterOpen && (
-          <motion.div
-            initial={{opacity: 0, y: 1000}}
-            animate={{opacity: 1, y: 0}}
-            exit={{opacity: 0, y: 1000}}
-            transition={{duration: 0.5}}
-            className='modal'
-          >
-            <Filter closeModal={closeClose} />
-          </motion.div>
-        )}
-      </AnimatePresence>
+          <AnimatePresence>
+            {isFilterOpen && (
+              <motion.div
+                initial={{opacity: 0, y: 1000}}
+                animate={{opacity: 1, y: 0}}
+                exit={{opacity: 0, y: 1000}}
+                transition={{duration: 0.5}}
+                className='modal'
+              >
+                <Filter closeModal={closeClose} />
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </>
+      ) : (
+        <>
+          <div className=' main__load'>
+            <Loader className='animate-spin-slow spinner' size={50} />
+            <p>Данная версия доступна только при использовании телеграма</p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
