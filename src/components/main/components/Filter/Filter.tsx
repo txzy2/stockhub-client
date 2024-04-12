554df897b5749b241866ce9fab15f888c146486c
anton <kamaeff2@gmail.com>
2024-04-12 15:27:23 +0300

    add inputs to filters modal

--- src/components/main/components/Filter/Filter.tsx
+++ src/components/main/components/Filter/Filter.tsx
@@ -10,46 +10,61 @@ const Filter = ({closeModal}: ModalProps) => {
         <X className='exit' size={30} />
       </button>
 
-      <div className='filter__options'>
-        <label className='grid'>
-          Одежда / Обувь
-          <select className='filter__options--select'>
-            <option value='cloth'>Одежда</option>
-            <option value='sneaker'>Обувь</option>
-          </select>
-        </label>
-      </div>
+      <form action=''>
+        <div className='filter__options'>
+          <label className='grid'>
+            Одежда / Обувь
+            <select className='filter__options--select'>
+              <option value='cloth'>Одежда</option>
+              <option value='sneaker'>Обувь</option>
+            </select>
+          </label>
+        </div>
 
-      <div className='filter__options'>
-        <label className='grid'>
-          Цвет
-          <select className='filter__options--select'>
-            <option value='gray'>Серый</option>
-            <option value='red'>Красный</option>
-          </select>
-        </label>
-      </div>
+        <div className='filter__options'>
+          <label className='grid'>
+            Цвет
+            <select className='filter__options--select'>
+              <option value='gray'>Серый</option>
+              <option value='red'>Красный</option>
+            </select>
+          </label>
+        </div>
 
-      <div className='filter__options'>
-        <label className='grid'>
-          Бренд
-          <select className='filter__options--select'>
-            <option value='nike'>Nike</option>
-            <option value='puma'>Puma</option>
-            <option value='jordan'>Jordan</option>
-          </select>
-        </label>
-      </div>
+        <div className='filter__options'>
+          <label className='grid'>
+            Бренд
+            <select className='filter__options--select'>
+              <option value='nike'>Nike</option>
+              <option value='puma'>Puma</option>
+              <option value='jordan'>Jordan</option>
+            </select>
+          </label>
+        </div>
 
-      <div className='filter__options'>
-        <label className='grid'>
-          Откуда
-          <select className='filter__options--select'>
-            <option value='msk'>MCK</option>
-            <option value='poizon'>Poizon</option>
-          </select>
-        </label>
-      </div>
+        <div className='filter__options'>
+          <label className='grid'>
+            Откуда
+            <select className='filter__options--select'>
+              <option value='msk'>MCK</option>
+              <option value='poizon'>Poizon</option>
+            </select>
+          </label>
+        </div>
+
+        <div className='filter__options_price'>
+          <input
+            className='filter__options_price--input'
+            type='number'
+            placeholder='От'
+          />
+          <input
+            className='filter__options_price--input'
+            type='number'
+            placeholder='До'
+          />
+        </div>
+      </form>
     </div>
   );
 };
