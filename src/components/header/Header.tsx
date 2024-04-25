fa7c947e52b5624b91d078168930c2e464983a27
anton <kamaeff2@gmail.com>
2024-04-25 18:25:24 +0300

    new animation fir header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -72,9 +72,9 @@ const Header = () => {
       <AnimatePresence>
         {isModalOpen && (
           <motion.div
-            initial={{opacity: 0, y: 1000}}
-            animate={{opacity: 1, y: 0}}
-            exit={{opacity: 0, y: 1000}}
+            initial={{opacity: 0, x: -1000}}
+            animate={{opacity: 1, x: 0}}
+            exit={{opacity: 0, x: -1000}}
             transition={{duration: 0.5}}
             className='modal'
           >
