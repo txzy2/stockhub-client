d5430d644a4e1ccad500d94d297cc4ee2942550c
anton <kamaeff2@gmail.com>
2024-05-24 17:32:50 +0300

    dix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect} from 'react';
 import './App.scss';
 
-import {Header, Main} from './components';
+import {Footer, Header, Main} from './components';
 import Circles from './components/ui/Circles';
 import {UseTg} from './hooks/useTg';
+import {images} from './assets/imagesAssets';
 
 const App = () => {
   const {tg, user} = UseTg();
@@ -14,40 +15,40 @@ const App = () => {
 
   return (
     <div className="App">
-      {/*{user?.id ? (*/}
-      <>
-        <div className="background">
-          <Circles />
-        </div>
-        <Header />
-        <Main />
-      </>
-      {/*) : (*/}
-      {/*  <>*/}
-      {/*    <div className="background">*/}
-      {/*      <Circles />*/}
-      {/*    </div>*/}
+      {user?.id ? (
+        <>
+          <div className="background">
+            <Circles />
+          </div>
+          <Header />
+          <Main />
+        </>
+      ) : (
+        <>
+          <div className="background">
+            <Circles />
+          </div>
 
-      {/*    <div className="main__load">*/}
-      {/*      <p className="error bold">StockHub12</p>*/}
-      {/*      <a*/}
-      {/*        href="https://t.me/yokross_bot"*/}
-      {/*        target="_blank"*/}
-      {/*        rel="noopener noreferrer"*/}
-      {/*      >*/}
-      {/*        <img*/}
-      {/*          className="qr"*/}
-      {/*          src={images.qr}*/}
-      {/*          alt="qr"*/}
-      {/*          width={300}*/}
-      {/*          height={300}*/}
-      {/*        />*/}
-      {/*      </a>*/}
-      {/*      <p className="error">Эксклюзиво только в Telegram</p>*/}
-      {/*    </div>*/}
-      {/*    <Footer />*/}
-      {/*  </>*/}
-      {/*)}*/}
+          <div className="main__load">
+            <p className="error bold">StockHub12</p>
+            <a
+              href="https://t.me/yokross_bot"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className="qr"
+                src={images.qr}
+                alt="qr"
+                width={300}
+                height={300}
+              />
+            </a>
+            <p className="error">Эксклюзиво только в Telegram</p>
+          </div>
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
