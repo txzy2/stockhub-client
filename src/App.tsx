7daf9fd4d14480a3a1c5fa3ba67eaee10e1bb9c8
anton <kamaeff2@gmail.com>
2024-05-24 17:55:07 +0300

    dix

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import {Footer, Header, Main} from './components';
 import Circles from './components/ui/Circles';
 import {UseTg} from './hooks/useTg';
 import {images} from './assets/imagesAssets';
+import {motion} from 'framer-motion';
+import Typewriter from 'typewriter-effect';
 
 const App = () => {
   const {tg, user} = UseTg();
@@ -15,37 +17,56 @@ const App = () => {
 
   return (
     <div className="App">
-      {user?.id ? (
+      <div className="background">
+        <Circles />
+      </div>
+      {user?.id && tg.platform !== 'tdesktop' ? (
         <>
-          <div className="background">
-            <Circles />
-          </div>
+
           <div className="">{tg.platform}</div>
           <Header />
           <Main />
         </>
       ) : (
         <>
-          <div className="background">
-            <Circles />
-          </div>
-
           <div className="main__load">
-            <p className="error bold">StockHub12</p>
-            <a
-              href="https://t.me/yokross_bot"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="qr"
-                src={images.qr}
-                alt="qr"
-                width={300}
-                height={300}
+            <div className="error bold">
+              <Typewriter
+                onInit={typewriter => {
+                  typewriter
+                    .typeString(`StockHub12`)
+                    .start()
+                    .callFunction(() => {
+                      (
+                        document.getElementsByClassName(
+                          'Typewriter__cursor'
+                        )[0] as HTMLElement
+                      ).style.display = 'none';
+                    });
+                }}
               />
-            </a>
-            <p className="error">Эксклюзиво только в Telegram</p>
+            </div>
+
+            <motion.div
+              whileHover={{scale: 1.03}}
+              transition={{type: 'spring', stiffness: 400, damping: 10}}
+            >
+              <a
+                href="https://t.me/yokross_bot"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="qr"
+                  src={images.qr}
+                  alt="qr"
+                  width={300}
+                  height={300}
+                />
+              </a>
+            </motion.div>
+            <div className="error">Эксклюзиво только в мобильном <a
+              href={'https://t.me/stockhub12bot'}>Telegram</a></div>
           </div>
           <Footer />
         </>
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,18 @@
+import {motion} from 'framer-motion';
 import './footer.scss';
 
 const Footer = () => {
-  return <div className='footer'>@StockHub12</div>;
+  return (
+    <div className="footer">
+      <motion.div
+        whileHover={{scale: 1.1}}
+        transition={{type: 'spring', stiffness: 400, damping: 10}}
+      >
+        <a href={'mailto:support@stockhub12.ru'}>@StockHubTech12</a>
+      </motion.div>
+    </div>
+
+  );
 };
 
 export default Footer;
