aff7101563789c34161046596b668043ecacec69
anton <kamaeff2@gmail.com>
2024-05-24 18:01:07 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, {useEffect} from 'react';
 import './App.scss';
+import {motion} from 'framer-motion';
 
-import {Header, Main} from './components';
+import {Footer, Header, Main} from './components';
 import Circles from './components/ui/Circles';
 import {UseTg} from './hooks/useTg';
+import Typewriter from 'typewriter-effect';
+import {images} from './assets/imagesAssets';
 
 const App = () => {
   const {tg, user} = UseTg();
@@ -17,55 +20,55 @@ const App = () => {
       <div className="background">
         <Circles />
       </div>
-      {/*{user?.id && tg.platform !== 'tdesktop' ? (*/}
-      <>
-        <Header />
-        <Main />
-      </>
-      {/*) : (*/}
-      {/*  <>*/}
-      {/*    <div className="main__load">*/}
-      {/*      <div className="error bold">*/}
-      {/*        <Typewriter*/}
-      {/*          onInit={typewriter => {*/}
-      {/*            typewriter*/}
-      {/*              .typeString(`StockHub12`)*/}
-      {/*              .start()*/}
-      {/*              .callFunction(() => {*/}
-      {/*                (*/}
-      {/*                  document.getElementsByClassName(*/}
-      {/*                    'Typewriter__cursor'*/}
-      {/*                  )[0] as HTMLElement*/}
-      {/*                ).style.display = 'none';*/}
-      {/*              });*/}
-      {/*          }}*/}
-      {/*        />*/}
-      {/*      </div>*/}
+      {user?.id && tg.platform !== 'tdesktop' ? (
+        <>
+          <Header />
+          <Main />
+        </>
+      ) : (
+        <>
+          <div className="main__load">
+            <div className="error bold">
+              <Typewriter
+                onInit={typewriter => {
+                  typewriter
+                    .typeString(`StockHub12`)
+                    .start()
+                    .callFunction(() => {
+                      (
+                        document.getElementsByClassName(
+                          'Typewriter__cursor'
+                        )[0] as HTMLElement
+                      ).style.display = 'none';
+                    });
+                }}
+              />
+            </div>
 
-      {/*      <motion.div*/}
-      {/*        whileHover={{scale: 1.03}}*/}
-      {/*        transition={{type: 'spring', stiffness: 400, damping: 10}}*/}
-      {/*      >*/}
-      {/*        <a*/}
-      {/*          href="https://t.me/yokross_bot"*/}
-      {/*          target="_blank"*/}
-      {/*          rel="noopener noreferrer"*/}
-      {/*        >*/}
-      {/*          <img*/}
-      {/*            className="qr"*/}
-      {/*            src={images.qr}*/}
-      {/*            alt="qr"*/}
-      {/*            width={300}*/}
-      {/*            height={300}*/}
-      {/*          />*/}
-      {/*        </a>*/}
-      {/*      </motion.div>*/}
-      {/*      <div className="error">Эксклюзиво только в мобильном <a*/}
-      {/*        href={'https://t.me/stockhub12bot'}>Telegram</a></div>*/}
-      {/*    </div>*/}
-      {/*    <Footer />*/}
-      {/*  </>*/}
-      {/*)}*/}
+            <motion.div
+              whileHover={{scale: 1.03}}
+              transition={{type: 'spring', stiffness: 400, damping: 10}}
+            >
+              <a
+                href="https://t.me/yokross_bot"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="qr"
+                  src={images.qr}
+                  alt="qr"
+                  width={300}
+                  height={300}
+                />
+              </a>
+            </motion.div>
+            <div className="error">Эксклюзиво только в мобильном <a
+              href={'https://t.me/stockhub12bot'}>Telegram</a></div>
+          </div>
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
