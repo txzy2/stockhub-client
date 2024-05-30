f5b0ea445b8ae45838d6dcec15fd2bc18a89bd09
anton <kamaeff2@gmail.com>
2024-05-30 15:09:04 +0300

    fix filter

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import {UseTg} from '../../hooks/useTg';
 const Footer = () => {
   const {user} = UseTg();
   return (
-    <div className={user.id ? 'footer' : 'footer-desk'}>
+    <div className={user?.id ? 'footer' : 'footer-desk'}>
       <a href={'mailto:support@stockhub12.ru'}>@StockHub12</a>
       <div className="footer__info">
         <a href="https://telegra.ph/Dogovor-oferty-na-okazanie-uslugi-11-27" target={'_blank'}>Договор
