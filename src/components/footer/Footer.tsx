edbdaeffdf7c66ec5b2e32281d438566388b5a49
anton <kamaeff2@gmail.com>
2024-05-30 14:08:57 +0300

    fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="footer__info">
         <a href="https://telegra.ph/Dogovor-oferty-na-okazanie-uslugi-11-27">Договор оферты</a>
         <a href="https://telegra.ph/Instrukciya-po-ispolzovaniyu-StockHubBot-12-13">Иструкция пользователя</a>
-        <a href="mailto:support@stockhub12.ru">Тех поддержка</a>
+        <a href="mailto:support@stockhub12.ru">support@stockhub12.ru</a>
       </div>
     </div>
 
